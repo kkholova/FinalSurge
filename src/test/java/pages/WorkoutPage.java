a8735e5a657ce41a8252738a8f80ad2912976b84
kkholova <k.kholova@godeltech.com>
2021-07-22 18:15:19 +0300

    Add new Type + small changes

--- src/test/java/pages/WorkoutPage.java
+++ src/test/java/pages/WorkoutPage.java
@@ -94,8 +94,12 @@ public class WorkoutPage extends BasePage {
     @Step("Check that workout with {workoutTitle} was deleted from the calendar")
     public boolean checkWorkoutWasDeletedFromCalendar(String workoutTitle) {
         log.info("Check workout with name " + workoutTitle + " was deleted");
-        driver.findElement(By.xpath(String.format(calendarTitleLocator, workoutTitle))).isDisplayed();
-        return false;
+        try {
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(String.format(calendarTitleLocator, workoutTitle))));
+            return driver.findElement(By.xpath(String.format(calendarTitleLocator, workoutTitle))).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
 }
